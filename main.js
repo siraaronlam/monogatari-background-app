ab25ebd3733f962c7a31cdbceeb9096c1b2f549b
Aaron <aaronl@ifit.com>
2020-03-16 23:42:58 -0600

    basis for a system tray application

--- main.js
+++ main.js
@@ -1,43 +1,62 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain, Tray } = require('electron');
+const path = require('path');
+require('dotenv').config();
 
-function createWindow () {
-  // Create the browser window.
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+let tray = undefined;
+let window = undefined;
 
-  // and load the index.html of the app.
-  win.loadFile('index.html')
+// Don't show the app in the doc
+app.dock.hide();
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
+const createWindow = () => {
+  window = new BrowserWindow({
+      width: 320,
+      height: 450,
+      show: false,
+      frame: false,
+      fullscreenable: false,
+      resizable: false,
+      transparent: false,
+  });
+  window.loadFile('index.html');
+
+  // Hide the window when it loses focus
+  window.on('blur', () => {
+      if (!window.webContents.isDevToolsOpened()) {
+          window.hide();
+      }
+  });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
+const getWindowPosition = () => {
+  const windowBounds = window.getBounds();
+  const trayBounds = tray.getBounds();
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+  // Center window horizontally below the tray icon
+  const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2))
+  // Position window 4 pixels vertically below the tray icon
+  const y = Math.round(trayBounds.y + trayBounds.height + 4)
+  return {x: x, y: y}
+}
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
+const showWindow = () => {
+  const position = getWindowPosition();
+  window.setPosition(position.x, position.y, false);
+  window.show();
+}
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+const toggleWindow = () => {
+  window.isVisible() ? window.hide() : showWindow();
+}
+
+const createTray = () => {
+  tray = new Tray(path.join('icon.png'));
+  tray.on('click', function (event) {
+      toggleWindow();
+  })
+}
+
+app.on('ready', () => {
+    createTray();
+    createWindow();
+});
