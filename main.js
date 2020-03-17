14484b8ff7815ff91f29d37cce339a0d81a8e643
Aaron <aaronl@ifit.com>
2020-03-17 00:42:42 -0600

    basis for downloading latest image from twitter

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-const { app, BrowserWindow, ipcMain, Tray } = require('electron');
-const path = require('path');
 require('dotenv').config();
+const { app, BrowserWindow, ipcMain, Tray } = require('electron');
+const fs = require('fs');
+const path = require('path');
+const request = require('request');
+const Twitter = require('twitter');
+
+const twitterClient = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_API_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_API_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKE_SECRET
+});
 
 let tray = undefined;
 let window = undefined;
@@ -10,23 +20,23 @@ app.dock.hide();
 
 const createWindow = () => {
   window = new BrowserWindow({
-      width: 320,
-      height: 450,
-      show: false,
-      frame: false,
-      fullscreenable: false,
-      resizable: false,
-      transparent: false,
+    width: 320,
+    height: 450,
+    show: false,
+    frame: false,
+    fullscreenable: false,
+    resizable: false,
+    transparent: false,
   });
   window.loadFile('index.html');
 
   // Hide the window when it loses focus
   window.on('blur', () => {
-      if (!window.webContents.isDevToolsOpened()) {
-          window.hide();
-      }
+    if (!window.webContents.isDevToolsOpened()) {
+      window.hide();
+    }
   });
-}
+};
 
 const getWindowPosition = () => {
   const windowBounds = window.getBounds();
@@ -37,26 +47,43 @@ const getWindowPosition = () => {
   // Position window 4 pixels vertically below the tray icon
   const y = Math.round(trayBounds.y + trayBounds.height + 4)
   return {x: x, y: y}
-}
+};
 
 const showWindow = () => {
   const position = getWindowPosition();
   window.setPosition(position.x, position.y, false);
   window.show();
-}
+};
 
 const toggleWindow = () => {
   window.isVisible() ? window.hide() : showWindow();
-}
+};
 
 const createTray = () => {
   tray = new Tray(path.join('icon.png'));
   tray.on('click', function (event) {
-      toggleWindow();
+    toggleWindow();
   })
-}
+};
+
+const downloadImage = (uri, filename) => {
+  request.head(uri, (err, res, body) => {
+    request(uri).pipe(fs.createWriteStream(filename));
+  });
+};
+
+const getTwitterResponse = async () => {
+  var params = {screen_name: 'gatari_lines'};
+  twitterClient.get('statuses/user_timeline', params, async (error, tweets, response) => {
+    if (!error) {
+      const media = tweets[0].entities.media;
+      await downloadImage(media[0].media_url_https, 'test.png');
+    }
+  });
+};
 
 app.on('ready', () => {
-    createTray();
-    createWindow();
+  getTwitterResponse();
+  createTray();
+  createWindow();
 });
